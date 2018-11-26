7a41e5a14e097f44079074ec2e770416578aae1d
Sandro <sandro.almeida@dotbuild.com.br>
2018-11-26 13:47:50 -0800

    Initial commit

--- src/main/java/com/sandroalmeida/controller/DemoController.java
+++ src/main/java/com/sandroalmeida/controller/DemoController.java
@@ -1,9 +1,13 @@
 package com.sandroalmeida.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class DemoController {
 
@@ -16,7 +20,19 @@ public class DemoController {
 
     // http://localhost:8080/todo-list/welcome
     @GetMapping("welcome")
-    public String welcome(){
+    public String welcome(Model model){
+
+        model.addAttribute("user", "Sandro");
+        log.info("model = {}", model);
+
+        // prefix + name + suffix
+        // /WEB-INF/view/welcome.jsp
         return "welcome";
+    }
+
+    @ModelAttribute("welcomeMessage")
+    public String welcomeMessage(){
+        log.info("welcomeMessage() called");
+        return "Welcome to this Demo Application.";
     }
 }
