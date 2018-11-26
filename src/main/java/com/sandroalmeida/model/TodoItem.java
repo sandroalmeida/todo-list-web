4f739b09bb2fb16d513f8563f12183a8a89140d5
Sandro <sandro.almeida@dotbuild.com.br>
2018-11-26 15:54:31 -0800

    Implement Model TodoItem and TodoData classes

---
+++ src/main/java/com/sandroalmeida/model/TodoItem.java
@@ -0,0 +1,26 @@
+package com.sandroalmeida.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode(of = "id")
+public class TodoItem {
+
+    // == fields ==
+    private int id;
+    private String title;
+    private String details;
+    private LocalDate deadline;
+
+    // == constructors ==
+    public TodoItem(String title, String details, LocalDate deadline) {
+        this.title = title;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+
+}
